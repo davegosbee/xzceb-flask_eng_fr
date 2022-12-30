92fc27c8d5c556cce6d6202e52eee218750eb41d
davegosbee <daveownsthis@hotmail.com>
2022-12-29 19:31:41 -0500

    tasks 1.9 to 1.11

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,10 +9,39 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
+authenticator = IAMAuthenticator(apikey)
+
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+#language_translator.set_disable_ssl_verification(True)
+
+
+
 def englishToFrench(englishText):
     #write the code here
+    translationResponse = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    
+    jsonResponse = json.loads(json.dumps(translationResponse))
+    frenchText = jsonResponse["translations"][0]["translation"]
+    
     return frenchText
 
 def frenchToEnglish(frenchText):
     #write the code here
+    translationResponse = language_translator.translate(
+    text=frenchText,
+    model_id='fr-en').get_result()
+    
+    jsonResponse = json.loads(json.dumps(translationResponse))
+    englishText = jsonResponse["translations"][0]["translation"]
+   
     return englishText
+
+print(englishToFrench("hello how are you"))
+print(frenchToEnglish('Bonjour comment vous êtes'))
